5d81cc9f5f0ca3941de8066f04e6db6c53b7d401
kimbirdwon <pswk0907@gmail.com>
2026-03-22 13:46:32 +0900

    Docker 컨테이너 설정

--- infra/terraform/azure/outputs.tf
+++ infra/terraform/azure/outputs.tf
@@ -7,3 +7,8 @@ output "consumer_nat_public_ip" {
   description = "NAT 공인 IP 주소"
   value       = azurerm_public_ip.consumer_nat_ip.ip_address
 }
+
+output "admin_username" {
+  description = "Broker VM에 접속하기 위한 관리자 계정명"
+  value       = azurerm_linux_virtual_machine.broker_vm.admin_username # VM 리소스에서 직접 가져옴
+}
